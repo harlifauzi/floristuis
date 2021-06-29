08f30e3681364116fa99a64b90097680125bd3c1
harleykwen <harlifauzi26@gmail.com>
2021-06-29 12:45:36 +0700

    implement atomic design on assets/image

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import HeroImage from "./hero.png";
+import ToolsImage from "./tools.png";
+import MoneyBouquet from "./money-bouquet.png";
+import FreshBouquet from "./fresh-bouquet.png";
+import WeddingStuff from "./wedding-stuff.png";
+
+export {
+    HeroImage,
+    ToolsImage,
+    MoneyBouquet,
+    FreshBouquet,
+    WeddingStuff
+}

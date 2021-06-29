dbf35bc0f48c7c2b752d71c1f63e4ca0a3859e5a
harleykwen <harlifauzi26@gmail.com>
2021-06-29 18:52:39 +0700

    create MyNavbar component

--- src/components/molecules/MyNavbar/index.js
+++ src/components/molecules/MyNavbar/index.js
@@ -3,8 +3,23 @@ import './MyNavbar.css';
 
 const MyNavbar = () => {
     return (
-        <div>
-            From MyNavbar
+        <div className="mynavbar-container">
+            <div className="mynavbar-wrapper">
+                <h1 className="mynavbar-logo">floristuis</h1>
+                <ul className="mynavbar-nav">
+                    <li>Products</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <ul className="mynavbar-nav-auth">
+                    <li>Signup</li>
+                    <li>Login</li>
+                </ul>
+                <label for="check">
+                    <i className='bx bx-menu-alt-right'></i>
+                </label>
+                <input type="checkbox" id="check" />
+            </div>
         </div>
     )
 }
